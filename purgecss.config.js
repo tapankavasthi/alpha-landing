92e21bb3c93c18645ce92044f2645afe1fc90f19
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-03-02 18:01:29 +0530

    Add purge css

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   content: ["./_site/**/*.html"],
-  css: ["./_site/css/site.css"],
+  css: ["./_site/css/tw_style.css"],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 };
