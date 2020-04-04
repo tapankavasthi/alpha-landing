5553141b8ebc8962106cec0ef68be5840639581c
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-04-04 17:54:08 +0530

    Add discord usernames too

--- scripts/list_completed.rb
+++ scripts/list_completed.rb
@@ -1,13 +1,19 @@
 require "pry"
 require "json"
 require "time"
+require "yaml"
 
 profiles = JSON.parse(File.read("_data/user_profiles.json"))
+
+github_discord_mapping = YAML.load(File.read("_data/github_discord_mapping.yml")).map { |k|
+  [k.fetch("github_username"), k.fetch("discord_username")]
+}.to_h
 
 users = profiles
   .map { |profile|
     {
-      username: profile["username"],
+      github_username: profile["username"],
+      discord_username: github_discord_mapping.fetch(profile["username"], "NIL"),
       completed_challenges: profile["events"]
         .select { |event| event["type"] == "completed-challenge" }
         .map { |event| event["challenge"] },
@@ -22,7 +28,7 @@ users = profiles
 users.each_with_index do |user, index| 
   puts [
     ((index+1).to_s + ".").ljust(3),
-    user[:username].ljust(20),
+    "#{user[:github_username]} (#{user[:discord_username]})".ljust(20),
     ":",
     user[:completed_challenges].join(", ").ljust(20),
     ((Time.now - user[:first_completion_at]) / (3600 * 24)).round(1),
